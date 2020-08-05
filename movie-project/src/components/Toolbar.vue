3508367acc8c26699bea2e64c5f218bda0f5957f
Andrew Power <andrewjosephpower@gmail.com>
2020-08-05 16:04:42 -0300

    Added toolbar

---
+++ movie-project/src/components/Toolbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-btn v-if="!root.ui.drawer.pinned" icon @click="toggleDrawer"></v-btn>
+      <h1>Data App</h1>
+    </div>
+
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  methods: {
+    toggleDrawer() {
+      this.$root.ui.drawer.visible = !this.$root.ui.drawer.visible;
+    },
+  },
+};
+</script>
