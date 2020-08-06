ce5c894bb6dca5754dd550ae6e49ac4177a7aa44
Andrew Power <andrewjosephpower@gmail.com>
2020-08-06 17:25:56 -0300

    Drawer working

--- movie-project/src/components/Drawer.vue
+++ movie-project/src/components/Drawer.vue
@@ -1,26 +1,97 @@
 <template>
-  <v-card height="400" width="256" class="mx-auto">
-    <v-navigation-drawer app src="../assets/popcorn.jpg" permanent expand-on-hover>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">Application</v-list-item-title>
-          <v-list-item-subtitle>subtext</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+  <v-navigation-drawer app src="../assets/popcorn.jpg" permanent expand-on-hover>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="title">Movie Player</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
 
-      <v-divider></v-divider>
+    <v-divider></v-divider>
 
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
+    <v-list dense nav>
+      <v-list-item-content>
+        <v-list-item-title>Collections</v-list-item-title>
+        <br />
+        <v-row justify="center">
+          <v-expansion-panels accordion>
+            <v-expansion-panel v-for="(item,i) in 1" :key="i">
+              <v-expansion-panel-header>Marvel</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <div class="left">
+                  <v-btn small block rounded color="primary" dark>Avengers</v-btn>
+                </div>
+                <div class="left">
+                  <v-btn small block rounded color="primary" dark>Avengers: Age of Ultron</v-btn>
+                </div>
+                <div class="left">
+                  <v-btn small block rounded color="primary" dark>Avengers: Infinity War</v-btn>
+                </div>
+                <div class="left">
+                  <v-btn small block rounded color="primary" dark>Avengers: Endgame</v-btn>
+                </div>
+                <div class="left">
+                  <v-btn small block rounded color="primary" dark>Guardians of the Galaxy</v-btn>
+                </div>
+                <div class="left">
+                  <v-btn small block rounded color="primary" dark>Guardians of the Galaxy 2</v-btn>
+                </div>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-row>
+        <v-row justify="center">
+          <v-expansion-panels accordion>
+            <v-expansion-panel v-for="(item,i) in 1" :key="i">
+              <v-expansion-panel-header>Star Wars</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <div class="left">
+                  <v-btn small block rounded color="primary" dark>Star Wars: The Phantom Menace</v-btn>
+                </div>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-row>
+        <v-row justify="center">
+          <v-expansion-panels accordion>
+            <v-expansion-panel v-for="(item,i) in 1" :key="i">
+              <v-expansion-panel-header>Animated</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <div class="left">
+                  <v-btn small block rounded color="primary" dark>Toy Story</v-btn>
+                </div>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-row>
+      </v-list-item-content>
+      <v-list-item-content>
+        <v-list-item-title>Movies</v-list-item-title>
+        <br />
+        <div class="left">
+          <v-btn small block rounded color="primary" dark>Avengers</v-btn>
+        </div>
+        <div class="left">
+          <v-btn small block rounded color="primary" dark>Avengers: Age of Ultron</v-btn>
+        </div>
+        <div class="left">
+          <v-btn small block rounded color="primary" dark>Avengers: Infinity War</v-btn>
+        </div>
+        <div class="left">
+          <v-btn small block rounded color="primary" dark>Avengers: Endgame</v-btn>
+        </div>
+        <div class="left">
+          <v-btn small block rounded color="primary" dark>Guardians of the Galaxy</v-btn>
+        </div>
+        <div class="left">
+          <v-btn small block rounded color="primary" dark>Guardians of the Galaxy 2</v-btn>
+        </div>
+        <div class="left">
+          <v-btn small block rounded color="primary" dark>Star Wars: The Phantom Menace</v-btn>
+        </div>
+        <div class="left">
+          <v-btn small block rounded color="primary" dark>Toy Story</v-btn>
+        </div>
+      </v-list-item-content>
+    </v-list>
+  </v-navigation-drawer>
 </template>
--- movie-project/src/components/Toolbar.vue
+++ movie-project/src/components/Toolbar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app-bar app color="primary" dark>
-    <div class="d-flex align-center">
-      <h1>Data App</h1>
+    <div class="text-center">
+      <h1>Disney Movie Player</h1>
     </div>
 
     <v-spacer></v-spacer>
